a1bc5b0eb7ecd3cdb2fc1d97f26011689cb9d556
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-03-10 17:22:43 -0300

    Application atualizada.

--- src/main/java/br/com/example/Application.java
+++ src/main/java/br/com/example/Application.java
@@ -2,9 +2,7 @@ package br.com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactivefeign.spring.config.EnableReactiveFeignClients;
 
-@EnableReactiveFeignClients
 @SpringBootApplication
 public class Application {
 
