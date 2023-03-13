9365a37210c8b02cd5605c6aa97d5f29ce665509
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-03-13 16:56:10 -0300

    ViaCepService atualizado.

--- src/main/java/br/com/example/services/ViaCepService.java
+++ src/main/java/br/com/example/services/ViaCepService.java
@@ -1,12 +1,14 @@
 package br.com.example.services;
 
 import br.com.example.domain.Endereco;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 public class ViaCepService {
 
@@ -18,7 +20,10 @@ public class ViaCepService {
         return viaCepClient.get()
                 .uri("/{cep}/json", cep)
                 .retrieve()
-                .bodyToMono(Endereco.class);
+                .bodyToMono(Endereco.class)
+                .doOnSubscribe(s -> log.info("Consultando cep {}...", cep))
+                .doOnError(err -> log.error("Ocorreu um erro durante a consulta de CEP: {}", err.getMessage()))
+                .doOnNext(endereco -> log.info("{}", endereco));
     }
 
 }
